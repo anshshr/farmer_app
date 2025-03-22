80f6ef54c650a57e40470efe81731e59c9098874
Shubham-2004 <shubhambera2022@gmail.com>
2025-03-22 14:00:32 +0530

    chnage the splash screen

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -8,12 +11,7 @@ plugins {
 android {
     namespace = "com.example.farmer_app"
     compileSdk = flutter.compileSdkVersion
-<<<<<<< HEAD
-    // ndkVersion = flutter.ndkVersion
     ndkVersion = "28.0.13004108"
-=======
-    ndkVersion = "27.0.12077973"
->>>>>>> 027a9cbd11a7e255f3811a9ba7692f478a324b81
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
