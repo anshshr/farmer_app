013023aaccc068113ca7ee25978692bf31b4c249
anshshr <anshshrivastav24@gmail.com>
2025-03-22 11:45:07 +0530

    start

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
-import materail.dart
+import "package:farmer_app/pages/auth/register.dart";
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "farmer app",
+      home: Register(),
+    ),
+  );
+}
